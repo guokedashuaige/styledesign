572514ad434b05707b6c92258aa92bf6aef2f0f2
Mageluer Linus <songff@yusur.tech>
2020-07-17 20:23:27 +0800

    Signed-off-by: Mageluer Linus <songff@yusur.tech>

---
+++ style design/NavTab/NavTab.js
@@ -0,0 +1,10 @@
+"use strict";
+var navtab = document.querySelector("nav.navtab");
+var navtabItems = document.querySelectorAll("li.navtab-item");
+navtabItems.forEach(function (navtabItem, activeIndex) {
+    return navtabItem.addEventListener("click", function () {
+        navtabItems.forEach(function (navtabItem) { return navtabItem.classList.remove("active"); });
+        navtabItem.classList.add("active");
+        navtab.style.setProperty("--active-index", "" + activeIndex);
+    });
+});
